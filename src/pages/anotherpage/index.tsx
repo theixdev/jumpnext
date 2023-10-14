14be21997ed8613dbf1821497ccf6012be7d920e
Dale Chester <77970786+theixdev@users.noreply.github.com>
2023-10-14 20:18:59 +1000

    Fixed ' in html

--- src/pages/anotherpage/index.tsx
+++ src/pages/anotherpage/index.tsx
@@ -1,9 +1,9 @@
 const AnotherPage = () => {
     return (
         <div className="flex-grow flex items-center justify-center">
-            <p>You don't need to log in to view this.</p>
+            <p>You dont need to log in to view this.</p>
         </div>
-    )
-}
+    );
+};
 
-export default AnotherPage
+export default AnotherPage;
